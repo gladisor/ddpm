60e7c525083adccde65a68f38b6b732f3192dec4
Tristan Shah <123.tristan.shah@gmail.com>
2024-05-03 14:48:35 -0700

    finished unet

--- scripts/validate.py
+++ scripts/validate.py
@@ -54,7 +54,7 @@ class Block(nn.Module):
         super().__init__()
 
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size = 3, padding = 1)
-        self.norm = nn.GroupNorm(8, out_channels)
+        self.norm = nn.GroupNorm(4, out_channels)
         self.act = nn.SiLU()
 
     def forward(self, x: Tensor, scale: Tensor = None, shift: Tensor = None) -> Tensor:
@@ -64,11 +64,10 @@ class Block(nn.Module):
         return self.act(x)
     
 class ResBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, time_emb_dim: int, groups: int = 8):
+    def __init__(self, in_channels: int, out_channels: int, time_emb_dim: int):
         super().__init__()
 
         self.time_mlp = nn.Sequential(nn.SiLU(), nn.Linear(time_emb_dim, out_channels * 2))
-        
         self.block1 = Block(in_channels, out_channels)
         self.block2 = Block(out_channels, out_channels)
         self.skip = nn.Conv2d(in_channels, out_channels, kernel_size = 1)
@@ -80,15 +79,19 @@ class ResBlock(nn.Module):
 
         h = self.block1(x, scale, shift)
         return self.block2(h) + self.skip(x)
-
+    
 class PreNorm(nn.Module):
+    '''
+    Normalizes the channels of an image and then applies the layer.
+    Performs a skip connection.
+    '''
     def __init__(self, layer: nn.Module, in_channels: int):
         super().__init__()
         self.norm = nn.GroupNorm(1, in_channels)
         self.layer = layer
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.layer(self.norm(x))
+        return self.layer(self.norm(x)) + x
     
 ## https://huggingface.co/blog/annotated-diffusion
 class Attention(nn.Module):
@@ -133,7 +136,7 @@ class Attention(nn.Module):
     
 ## https://huggingface.co/blog/annotated-diffusion
 class LinearAttention(nn.Module):
-    def __init__(self, in_channels: int, heads: int = 4, dim_head: int = 32):
+    def __init__(self, in_channels: int, heads: int = 2, dim_head: int = 32):
         super().__init__()
         self.heads = heads
         self.scale = dim_head ** -0.5
@@ -163,13 +166,119 @@ class LinearAttention(nn.Module):
         out = rearrange(out, 'b h c (x y) -> b (h c) x y', x = h, y = w)
         return self.output(out)
     
-class UNet(nn.Module()):
-    def __init__(self):
+class UNet(nn.Module):
+    def __init__(self, in_channels: int = 3, time_emb_dim: int = 160):
         super().__init__()
 
         self.time_mlp = nn.Sequential(
-            SinusoidalEmbeddings(160)
-        )
+            SinusoidalEmbeddings(time_emb_dim),
+            nn.Linear(time_emb_dim, time_emb_dim),
+            nn.GELU(),
+            nn.Linear(time_emb_dim, time_emb_dim)
+            )
+        
+        self.input_layer = nn.Conv2d(in_channels, 32, kernel_size = 1)
+
+        self.downs = nn.ModuleList([
+            nn.ModuleList([
+                ResBlock(32, 32, time_emb_dim),
+                ResBlock(32, 32, time_emb_dim),
+                PreNorm(LinearAttention(32), 32),
+                DownBlock(32, 32)
+            ]),
+
+            nn.ModuleList([
+                ResBlock(32, 32, time_emb_dim),
+                ResBlock(32, 32, time_emb_dim),
+                PreNorm(LinearAttention(32), 32),
+                DownBlock(32, 64)
+            ]),
+
+            nn.ModuleList([
+                ResBlock(64, 64, time_emb_dim),
+                ResBlock(64, 64, time_emb_dim),
+                PreNorm(LinearAttention(64), 64),
+                DownBlock(64, 128)
+            ]),
+
+            nn.ModuleList([
+                ResBlock(128, 128, time_emb_dim),
+                ResBlock(128, 128, time_emb_dim),
+                PreNorm(LinearAttention(128), 128),
+                nn.Conv2d(128, 256, kernel_size = 3, padding = 1)
+            ])
+        ])
+
+        ## bottleneck section of the UNet
+        self.mid_block1 = ResBlock(256, 256, time_emb_dim)
+        self.mid_attention = Attention(256) ## pixelwise attention
+        self.mid_block2 = ResBlock(256, 256, time_emb_dim)
+
+        self.ups = nn.ModuleList([
+            nn.ModuleList([
+                ResBlock(256 + 128, 256, time_emb_dim),
+                ResBlock(256 + 128, 256, time_emb_dim),
+                PreNorm(LinearAttention(256), 256),
+                UpBlock(256, 128)
+            ]),
+
+            nn.ModuleList([
+                ResBlock(128 + 64, 128, time_emb_dim),
+                ResBlock(128 + 64, 128, time_emb_dim),
+                PreNorm(LinearAttention(128), 128),
+                UpBlock(128, 64)
+            ]),
+
+            nn.ModuleList([
+                ResBlock(64 + 32, 64, time_emb_dim),
+                ResBlock(64 + 32, 64, time_emb_dim),
+                PreNorm(LinearAttention(64), 64),
+                UpBlock(64, 32)
+            ]),
+
+            nn.ModuleList([
+                ResBlock(32 + 32, 32, time_emb_dim),
+                ResBlock(32 + 32, 32, time_emb_dim),
+                PreNorm(LinearAttention(32), 32),
+                nn.Conv2d(32, 32, kernel_size = 3, padding = 1)
+            ])
+        ])
+        
+        self.output_res = ResBlock(64, 32, time_emb_dim)
+        self.output_layer = nn.Conv2d(32, in_channels, kernel_size = 1)
+
+    def forward(self, x: Tensor, t: Tensor) -> Tensor:
+        b, c, h, w = x.shape
+
+
+        time_emb = self.time_mlp(t)
+        y = self.input_layer(x) ## (b x 32 x 128 x 128)
+        r = y.clone()
+
+        residuals = []
+        for res1, res2, attention, downsample in self.downs:
+            y = res1(y, time_emb)
+            residuals.append(y)
+            y = res2(y, time_emb)
+            y = attention(y)
+            residuals.append(y)
+            y = downsample(y)
+
+        y = self.mid_block1(y, time_emb)
+        y = self.mid_attention(y)
+        y = self.mid_block2(y, time_emb)
+        ## (b x 256 x 16 x 16)
+
+        for res1, res2, attention, upsample in self.ups:
+            y = res1(torch.cat((y, residuals.pop()), dim = 1), time_emb)
+            y = res2(torch.cat((y, residuals.pop()), dim = 1), time_emb)
+            y = attention(y)
+            y = upsample(y)
+
+        y = self.output_res(torch.cat((y, r), dim = 1), time_emb)
+        y = self.output_layer(y)
+
+        return y
 
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -196,35 +305,12 @@ if __name__ == '__main__':
 
     # sampler.plot_forward(x0, 10, path = 'forward.png')
     t = torch.randint(0, timesteps, (x0.shape[0],)).long().to(device)
-    # time_emb_dim = 160
-    # emb = SinusoidalEmbeddings(time_emb_dim).to(device)
-    # layer = ResBlock(3, 16, time_emb_dim).to(device)
-    # down1 = DownBlock(16, 32).to(device)
-    # down2 = DownBlock(32, 64).to(device)
-    # down3 = DownBlock(64, 128).to(device)
-
-    # attention = Attention(32, 2).to(device)
-    # linear_attention = LinearAttention(32, 2).to(device)
-
-    # time_emb = emb(t)
-    # y = down1(layer(x0, time_emb))
 
     model = UNet()
-
-    model.time_mlp(t)
-
-    print(x0.shape)
-    print(t.shape)
-
-
-
-
-
-
-
-
-
+    model(x0, t)
+    # from unet_youtube import Unet
     # model = Unet(dim = 32).to(device)
+    # print(model.downs)
     # model.train()
 
     # pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
